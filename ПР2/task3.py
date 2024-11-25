31fe0e56242d2d6451b4151380ee91462b7a5d1a
Смирнов Степан и Сабельфельд Василий <foxelyss@gmail.com>
2024-11-25 13:50:49 +0700

    ПР 2 Задание 3

---
+++ ПР2/task3.py
@@ -0,0 +1,17 @@
+matrice = []
+
+n = 5
+
+for x in range(n):
+    array = []
+    for y in range(n):
+        array.append(1)
+    matrice.append(array)
+
+for x in range(1,n):
+    for y in range(1, n):
+        matrice[x][y] = matrice[x-1][y] + matrice[x][y-1]
+
+for x in matrice:
+    print(*x,sep='\t')
+
