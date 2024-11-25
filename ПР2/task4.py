9ea23e277218bf5dc069a3238cc46263dac71b1e
Смирнов Степан и Сабельфельд Василий <foxelyss@gmail.com>
2024-11-25 14:04:34 +0700

    ПР 2 Задание 4 Генераторам.Нет

--- ПР2/task4.py
+++ ПР2/task4.py
@@ -6,7 +6,14 @@ def average_temp(temperature):
         array.append(sum(temperature[i])/30)
     return array
 
-temperature = [[randint(0,31) for i in range(30)] for _ in range(12)]
+temperature = []
+
+for _ in range(12):
+    month = []
+    for i in range(30):
+        month.append(randint(0,31))
+    temperature.append(month)
+
 avarages_temperature = sorted(average_temp(temperature))
 
 print(avarages_temperature)
