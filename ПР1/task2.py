b7fb4931c740fce3c4ab28fa9b6167204b47d357
Смирнов Степан и Сабельфельд Василий <foxelyss@gmail.com>
2024-11-25 12:37:55 +0700

    ПР 1 Задание 2
    Signed-off-by: Смирнов Степан и Сабельфельд Василий <foxelyss@gmail.com>

---
+++ ПР1/task2.py
@@ -0,0 +1,24 @@
+sum = 0
+multi = 1
+
+def get_value():
+    try:
+        return int(input())
+    except:
+        print("Введите число")
+        return get_value()
+
+num = get_value()
+
+quantity = 0
+while num != 0:
+    sum += num
+    multi *= num
+
+    quantity += 1
+
+    num = get_value()
+
+print(sum)
+print(multi)
+print(sum/quantity)
