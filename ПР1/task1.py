70e4298886c37ea1e2a2682bd5487b00d75f3f93
Smirnoff Stepan & Sabelfeld Vasilii <foxelyss@gmail.com>
2024-11-25 12:55:48 +0700

    ПР 1 Задание 3
    Signed-off-by: Smirnoff Stepan & Sabelfeld Vasilii <foxelyss@gmail.com>

---
+++ ПР1/task1.py
@@ -0,0 +1,12 @@
+# Создайте коллекцию(массив) с 10 случайными числами.
+# Выведите в консоль номер минимального элемента
+import random
+
+array = []
+
+for x in range(10):
+    array.append(random.randint(-10, 10))
+
+minimal = min(array)
+print(array)
+print(array.index(minimal))
---
+++ ПР1/task3.py
@@ -0,0 +1,18 @@
+strings = []
+
+string = input()
+while string != "":
+    strings.append(string)
+    string = input()
+
+shortest_string = strings[0]
+longest_string = strings[0]
+
+for x in strings:
+    if len(x) > len(longest_string):
+        longest_string = x
+    elif len(x) < len(shortest_string):
+        shortest_string = x
+
+print("Самый короткий элемент массива: \"" + shortest_string +'"')
+print("Самый длинный элемент массива: \"" +longest_string +'"')
