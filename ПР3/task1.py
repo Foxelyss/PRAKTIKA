be7a23fa928e2198ae6c22c7db11eea119ce7a53
Степан Смирнов и Сабельфельд Василий <foxelyss@gmail.com>
2024-11-26 15:19:42 +0700

    ПР 3 Улучшение читаемости

--- ПР3/task1.py
+++ ПР3/task1.py
@@ -1,13 +1,18 @@
 with open("input.txt", "r") as input_file:
+
     lucky_numbers = list(map(int, input_file.readline().split()))
     number_of_participants = int(input_file.readline())
+
     with open("output.txt", "w+") as output_file:
         for x in range(number_of_participants):
+
             count = 0
             ticket = list(map(int, input_file.readline().split()))
+
             for i in lucky_numbers:
                 if i in ticket:
                     count += 1
+
             if count >= 3:
                 output_file.write("Lucky\n")
             else:
--- ПР3/task2.py
+++ ПР3/task2.py
@@ -1,5 +1,3 @@
-# 1 2 3 4 5 6 7 8 9 10 20 31 55 66 88 89 100
-
 with open("nums.txt", "r") as input_file:
     numbers = list(map(int, input_file.read().split()))
 
