e27ed9d5cc1b7fe6665e56e751c8b3ae27de71e9
Степан Смирнов <foxelyss@gmail.com>
2024-11-26 12:26:42 +0700

    Улучшение вывода в ПР 2 задании 1

--- ПР2/task1.py
+++ ПР2/task1.py
@@ -3,7 +3,7 @@ array = []
 for x in range(100, 0, -1):
     array.append(x * 3)
 
-for x in range(10):
-    for y in range(10):
-        print(array[x + y * 10], end='\t')
+for y in range(10):
+    for x in range(10):
+        print(array[x + y * 10], end=',\t')
     print()
