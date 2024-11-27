15bca23620ad0eeaba9dd18aaeb4488e67c68a93
Степан Смирнов и Сабельфельд Василий <foxelyss@gmail.com>
2024-11-27 16:36:28 +0700

    ПР 4 Рефактор задания 4

--- ПР4/task4.py
+++ ПР4/task4.py
@@ -1,19 +1,17 @@
 with open("numsTask4.txt", "r") as file:
     numbers = list(map(int, file.read().split()))
 
-numbers_quantity = set(numbers)
-nums = 1
+number_quantity = 1
 last_num = numbers[0]
 
-for i in range(1,len(numbers)):
+for i in range(1, len(numbers)):
     if last_num == numbers[i]:
-        nums += 1
+        number_quantity += 1
     else:
-        print(f"Число {last_num} стоит {nums} раза")
-        nums = 1
+        print(f"Число {last_num} стоит {number_quantity} раза")
+        number_quantity = 1
 
     if i == len(numbers) - 1:
-        print(f"Число {last_num} стоит {nums} раза")
+        print(f"Число {last_num} стоит {number_quantity} раза")
 
     last_num = numbers[i]
-
