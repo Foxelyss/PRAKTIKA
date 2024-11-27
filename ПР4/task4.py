a8a053b790a3ede9924d50c29537756698b09814
Степан Смирнов и Сабельфельд Василий <foxelyss@gmail.com>
2024-11-27 16:43:37 +0700

    ПР 4 Изменение 4-го задания

--- ПР4/task4.py
+++ ПР4/task4.py
@@ -1,17 +1,20 @@
 with open("numsTask4.txt", "r") as file:
     numbers = list(map(int, file.read().split()))
 
+repeating_numbers_quantity = 0
 number_quantity = 1
 last_num = numbers[0]
 
 for i in range(1, len(numbers)):
     if last_num == numbers[i]:
         number_quantity += 1
-    else:
-        print(f"Число {last_num} стоит {number_quantity} раза")
+    elif number_quantity != 1:
+        repeating_numbers_quantity += number_quantity
         number_quantity = 1
 
     if i == len(numbers) - 1:
-        print(f"Число {last_num} стоит {number_quantity} раза")
+        repeating_numbers_quantity += number_quantity
 
     last_num = numbers[i]
+
+print("Количество одинаковых рядом стоящих чисел:",repeating_numbers_quantity)
