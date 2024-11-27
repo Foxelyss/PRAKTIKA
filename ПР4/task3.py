fc0407bcb6dc995428eeddb9317cb8d133506b89
Степан Смирнов и Сабельфельд Василий <foxelyss@gmail.com>
2024-11-27 17:11:46 +0700

    ПР 4 Рефактор 3-го задания

--- ПР4/task3.py
+++ ПР4/task3.py
@@ -1,15 +1,15 @@
 with open("numsTask3.txt", "r") as file:
     numbers = list(map(int, file.read().split(",")))
 
-max_nums = 0
-min_nums = max(numbers)
+max_number = numbers[0]
+min_number = numbers[0]
 
 for i in numbers:
     if i == 0:
         break
-    elif i > max_nums:
-        max_nums = i
-    elif i < min_nums:
-        min_nums = i
+    elif i > max_number:
+        max_number = i
+    elif i < min_number:
+        min_number = i
 
-print("Отношение минимума к максимуму чисел до нуля", min_nums / max_nums)
+print("Отношение минимума к максимуму чисел до нуля", min_number / max_number)
