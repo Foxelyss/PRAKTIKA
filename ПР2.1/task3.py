1f9eb51135fbb3ed2d9f50127a0c96bb54b96c29
Stephan Smirnoff <foxelyss@gmail.com>
2025-04-28 08:59:39 +0700

    Решение задания 3 первого дня

--- ПР2.1/task3.py
+++ ПР2.1/task3.py
@@ -1,6 +1,15 @@
 # Задание 3. Дан целочисленный массив nums, верните true, если какое-либо значение
 # встречается в массиве по крайней мере дважды, иначе верните false.
-# Ввод: Вывод:
+# Ввод: | Вывод:
 # nums = [1,2,3,4] false
 # nums = [1,1,1,3,3,4,3,2,4,2] true
 # nums = [1,2,3,1] true
+
+nums = list(map(int,input("Введите числа через пробел: ").split()))
+
+not_reaccuring = set(nums)
+
+if len(nums) == len(not_reaccuring):
+    print("false")
+else:
+    print("true")
