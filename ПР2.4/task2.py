0eb121e9f8f13520cb2583f0fbb9802d1c12bf29
Stephan Smirnoff <foxelyss@gmail.com>
2025-05-07 12:05:30 +0700

    Чтение скрипта из файла

--- ПР2.4/task2.py
+++ ПР2.4/task2.py
@@ -18,76 +18,8 @@ import sqlite3
 
 con = sqlite3.connect("task2.db")
 
-create_table = """
-create table if not exists drink(
-id integer primary key,
-name char(26) not null,
-alcohol_percentage float(3) not null,
-price_for_litre float(2) not null
-);
-
-create table if not exists ingredient(
-id integer primary key,
-name char(26) not null
-);
-
-create table if not exists cocktails(
-id integer primary key,
-name char(26) not null,
-alcohol_percentage float(2) not null,
-price float(2) not null
-);
-
-create table if not exists ingredients(
-id integer primary key,
-cocktail int references cocktails(id),
-ingredient int references ingredient(id),
-volume float(3) not null
-);
-
-create table if not exists drinks(
-id integer primary key,
-cocktail int references cocktails(id),
-drink int references drink(id),
-volume float(3) not null
-);
-
-create table if not exists supply_ingredient(
-id integer primary key,
-supply_date date not null,
-ingredient int references ingredient(id),
-volume float(3) not null,
-quantity int not null,
-price float(2) not null
-);
-
-create table if not exists supply_drink(
-id integer primary key,
-supply_date date not null,
-drink int references drinks(id),
-volume float(3) not null,
-quantity int not null,
-price float(2) not null
-);
-
-create table if not exists sells_drink(
-id integer primary key,
-sell_date date not null,
-cocktail int references cocktails(id),
-volume float(3) not null,
-quantity int not null,
-price float(2) not null
-);
-
-create table if not exists sells_cocktail(
-id integer primary key,
-sell_date date not null,
-cocktail int references cocktails(id),
-volume float(3) not null,
-quantity int not null,
-price float(2) not null
-);
-"""
+with open('task2.sql', 'r') as file:
+    create_table = file.read()
 
 con.executescript(create_table)
 con.commit()
