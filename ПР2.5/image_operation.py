e0a5d16819585a6bf3902689818c279dc61d4f0e
Stephan Smirnoff <foxelyss@gmail.com>
2025-05-12 17:08:19 +0700

    Подсказки по типам полей

--- ПР2.5/image_operation.py
+++ ПР2.5/image_operation.py
@@ -1,6 +1,6 @@
 
 class ImageOperation:
-    def __init__(self, name, is_gif, processing_func):
+    def __init__(self, name:str, is_gif:bool, processing_func):
         self.name = name
         self.is_gif = is_gif
         self.processing_func = processing_func
