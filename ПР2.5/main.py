d3c81b89bb82f672fe9d9134b8205a521b8ad2ab
Stephan Smirnoff <foxelyss@gmail.com>
2025-05-13 10:09:18 +0700

    Добавление команды для случайного мема с реддита

--- ПР2.5/main.py
+++ ПР2.5/main.py
@@ -5,6 +5,8 @@ import textwrap
 import os
 
 import telebot
+import requests
+from datetime import datetime
 from PIL import Image, ImageFont, ImageDraw
 
 import image_manipulation
@@ -138,7 +140,7 @@ def make_suslik_meme(message: telebot.types.Message):
         bot.reply_to(message, "Нужно 3 строки с текстом")
         return
 
-    if len(message_parts[1]) + len(message_parts[2]) > 28*5 or len(message_parts[3]) > 28*5:
+    if len(message_parts[1]) + len(message_parts[2]) > 28 * 5 or len(message_parts[3]) > 28 * 5:
         bot.reply_to(message, "Строки чересчур длинные")
         return
 
@@ -155,13 +157,14 @@ def make_suslik_meme(message: telebot.types.Message):
     response = wrap(message_parts[3])
 
     draw.text((300, 0), question + "\n" + answer, (0, 0, 0), font=font,
-                        stroke_fill=(255, 255, 255), stroke_width=4, align='center', anchor="ma")
+              stroke_fill=(255, 255, 255), stroke_width=4, align='center', anchor="ma")
     draw.text((300, 350), response, (0, 0, 0), font=font,
               stroke_fill=(255, 255, 255), stroke_width=4, align='center', anchor="ma")
 
     image_buffer = image_manipulation.prepare_png(image)
 
     bot.send_photo(message.chat.id, image_buffer)
+
 
 @bot.message_handler(commands=['for_better'])
 def make_for_the_better_right_meme(message: telebot.types.Message):
@@ -171,7 +174,7 @@ def make_for_the_better_right_meme(message: telebot.types.Message):
         bot.reply_to(message, "Нужно 3 строки с текстом")
         return
 
-    if len(message_parts[1]) + len(message_parts[2]) > 28*5 or len(message_parts[3]) > 28*5:
+    if len(message_parts[1]) + len(message_parts[2]) > 28 * 5 or len(message_parts[3]) > 28 * 5:
         bot.reply_to(message, "Строки чересчур длинные")
         return
 
@@ -188,9 +191,9 @@ def make_for_the_better_right_meme(message: telebot.types.Message):
     response = wrap(message_parts[3])
 
     draw.text((285, 0), question, (0, 0, 0), font=font,
-                        stroke_fill=(255, 255, 255), stroke_width=4, align='center', anchor="ma")
-    draw.text((855, 0),  answer, (0, 0, 0), font=font,
-                        stroke_fill=(255, 255, 255), stroke_width=4, align='center', anchor="ma")
+              stroke_fill=(255, 255, 255), stroke_width=4, align='center', anchor="ma")
+    draw.text((855, 0), answer, (0, 0, 0), font=font,
+              stroke_fill=(255, 255, 255), stroke_width=4, align='center', anchor="ma")
     draw.text((855, 570), response, (0, 0, 0), font=font,
               stroke_fill=(255, 255, 255), stroke_width=4, align='center', anchor="ma")
 
@@ -198,5 +201,41 @@ def make_for_the_better_right_meme(message: telebot.types.Message):
 
     bot.send_photo(message.chat.id, image_buffer)
 
+
+@bot.message_handler(commands=['programming_meme'])
+def make_for_the_better_right_meme(message: telebot.types.Message):
+    image = get_meme_image()
+    image_title = image["title"].replace("<", "&lt;").replace(">", "&gt;").replace("&", "&amp;")
+
+    bot.send_photo(message.chat.id, image["url"],
+                   caption=f"{image_title}\nАвтор: {image["author"]}\n<a href=\"{image["postLink"]}\">Клик на пост!</a>",
+                   parse_mode="HTML")
+
+
+def get_meme_image():
+    history = load_history()
+
+    current_date = datetime.today()
+
+    if (current_date - datetime.strptime(history["date"], "%Y-%m-%d")).days > 8:
+        base_url = 'https://memesapi.vercel.app/give/ProgrammerHumor/100'
+        response = requests.get(base_url)
+        data = response.json()
+        data["date"] = str(current_date)
+
+        with open("meme_cache.json", "w", encoding='utf8') as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
+
+        return random.choice(data["memes"])
+
+    return random.choice(history["memes"])
+
+
+def load_history():
+    try:
+        with open("meme_cache.json", "r", encoding='utf8') as file:
+            return json.load(file)
+    except:
+        return {"date": "2007-01-01"}
 
 bot.infinity_polling()
